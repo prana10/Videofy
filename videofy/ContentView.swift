40b6c1fa3e3e065bfba65432eee9663665d90acc
prana <pranadhika22@gmail.com>
2024-08-23 09:36:25 +0700

    Initial Commit

---
+++ videofy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  videofy
+//
+//  Created by Prana dhika on 23/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ videofy/videofyApp.swift
@@ -0,0 +1,17 @@
+//
+//  videofyApp.swift
+//  videofy
+//
+//  Created by Prana dhika on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct videofyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
